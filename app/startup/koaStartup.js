8f3119d16469dabac7ef65d40f099ad2e572b477
MD Eesha <eesha@thinkjs.co>
2022-12-03 01:48:42 +0530

    --aaded app.use(json)

--- app/startup/koaStartup.js
+++ app/startup/koaStartup.js
@@ -4,6 +4,7 @@ const dbMigrations = require("../utils/dbMigrations");
 const dotenv = require('dotenv').config();
 const CONFIG = require('../../config')
 const Router = require('koa-router')
+const json = require('koa-json');
 const KoaBodyParser = require('koa-bodyparser');
 const koaHelmet = require('koa-helmet')
 const KoaResponseTime = require('koa-response-time')
@@ -11,6 +12,7 @@ const allRoutes = require('../routes/index')
 // const KoaLogger = require('koa-logger')
     
 module.exports = async function (app) {    
+    app.use(json())
     app.use(KoaBodyParser())
     app.use(KoaResponseTime())
     // app.use(KoaLogger((str) => { logger.info(str) }))
@@ -20,7 +22,7 @@ module.exports = async function (app) {
 
     // initialize mongodb 
     await require('./db_mongo')();
-    
+
     app.listen(process.env.SERVER_PORT,()=>{console.log(`Server is running at http://localhost:${process.env.SERVER_PORT}`)})
 
      /* -- database migration
